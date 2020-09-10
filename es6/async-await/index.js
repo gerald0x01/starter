6596edd03c8bf06ec08c1f34f020ab92e9714637
gerald0x01 <geraldoahnert@gmail.com>
2020-09-10 19:17:04 -0300

    Add: Something about async and await. | Remove: DOM files.

--- es6/async-await/index.js
+++ es6/async-await/index.js
@@ -1,3 +1,13 @@
-async function executaPromise() {
-    const responde = await minhaPromise();
+import axios from 'axios';
+
+class Api {
+    static getUserInfo(username) {
+        try {
+            const reponse = await axios.get(`https://locallhost/api/${username}`);
+        } catch (err) {
+            console.log(response);
+        }
+    }
 }
+
+Api.getUserInfo('geraldo');
